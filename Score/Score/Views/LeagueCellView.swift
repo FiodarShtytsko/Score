a32991b0304663f86e7f157a39bcfd466fd611eb
FiodarShtytsko <134956650+FiodarShtytsko@users.noreply.github.com>
2025-03-21 13:27:10 +0100

    made base expanding

--- Score/Score/Views/LeagueCellView.swift
+++ Score/Score/Views/LeagueCellView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import Kingfisher
 
 struct LeagueCellView: View {
-
+    @State private var isExpanded: Bool = false
     let league: League
 
     var body: some View {
@@ -18,13 +18,26 @@ struct LeagueCellView: View {
                 .resizable()
                 .frame(width: 24, height: 24)
             Text(league.name)
+                .foregroundStyle(.black)
                 .bold()
             Spacer()
-            Image(systemName: "chevron.right")
+            Image(systemName: isExpanded ? "chevron.down" : "chevron.right")
+                .animation(.easeInOut(duration: 0.35), value: isExpanded)
+        }
+        .contentShape(Rectangle())
+        .onTapGesture {
+            withAnimation(.easeInOut(duration: 0.35)) {
+                isExpanded.toggle()
+            }
         }
 
-        ForEach(league.matches, id: \.id) { match in
-            MatchCellView(match: match)
+        if isExpanded {
+            ForEach(league.matches, id: \.id) { match in
+                MatchCellView(match: match)
+                    .transition(.opacity)
+                    .animation(.easeInOut(duration: 0.35), value: isExpanded)
+            }
         }
     }
 }
+
